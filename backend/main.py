ab4a7b1556effb0b1f7db5a7cbd467ab66244414
Vinay8git <vinayyd277@gmail.com>
2024-10-09 22:02:39 +0530

    Updated APIs with Error codes

--- backend/main.py
+++ backend/main.py
@@ -20,20 +20,23 @@ def greet():
 def greetBYName(name : str):
     return f'hello{name}'
 
+#Read all todos
 @app.get('/api/v1/todos', status_code=status.HTTP_200_OK)
-def getAllTodos(limit=Query(...), offset=Query(...)) -> List[dict]:
+def get_all_todos(limit=Query(...), offset=Query(...)) -> List[dict]:
     return todos[offset:limit+offset]
 
+#Read Todo by Title
 @app.get('/api/v1/todos/title/{title}', status_code=status.HTTP_200_OK)
-def getAllTodos(title: str) -> dict:
+def get_todos_by_title(title: str) -> dict:
     for todo in todos:
         if todo['title'] == title:
             return todo
     raise HTTPException(status.HTTP_404_NOT_FOUND,
             f"Todo with title {title} not found")
 
+#Read todo by status
 @app.get('/api/v1/todos/status/{status}')
-def getAllTodos(status: str) -> List:
+def get_todos_by_status(status: str) -> List:
     result=[]
     for todo in todos:
         if todo['status'] == status:
@@ -54,16 +57,18 @@ def getAllTodos(status: str) -> List:
 # enforcing title to be unique -> user defined constraint
 # {"title": "wakeup","desc":"1" }
 # {"title": "wakeup","desc":"2" }
+
 @app.post('/api/v1/todos', status_code=status.HTTP_201_CREATED)
 def create_todo(todo: dict) -> bool:
-	if (getAllTodos(todo['title'])):
+	if (get_todos_by_title(todo['title'])):
 		
-		message = f"todo with title {todo['title']} already exists."
+		message = f"Todo with title {todo['title']} already exists."
 		raise HTTPException(status.HTTP_400_BAD_REQUEST,message)
 	todos.append(todo)
 	print(todos)
 	return True
 
+#Update A todo
 @app.put('/api/v1/todos')
 def update_todo(payload: dict) -> bool:
 	update_idx = -1
@@ -79,7 +84,7 @@ def update_todo(payload: dict) -> bool:
 	return True
 
 
-
+#Delete A Todo
 @app.delete('/api/v1/todos/delete-by-title/{title}')
 def delete_todo(title: str) -> bool:
 	delete_idx = -1
