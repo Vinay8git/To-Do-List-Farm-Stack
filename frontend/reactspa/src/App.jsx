3cebdcc9964c6bab482259729198ed193c5f78fc
Vinay8git <vinayyd277@gmail.com>
2024-10-10 21:10:02 +0530

    Frontend Update

---
+++ frontend/reactspa/src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name = 'MIT';
+  
+  return (
+    <div className="App">
+      <p>Hello @K@^ @ {name}</p>
+    </div>
+  );
+}
+
+export default App;
