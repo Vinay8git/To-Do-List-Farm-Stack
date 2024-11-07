28a55e975d9436361bfab767a1571dc804477811
Vinay8git <vinayyd277@gmail.com>
2024-11-07 19:07:01 +0530

    Updated UI

--- frontend/reactspa/src/App.jsx
+++ frontend/reactspa/src/App.jsx
@@ -8,18 +8,14 @@ function App() {
   // function greet(){
   //   alert('Ready!')
   // }
-
+  let todosList = {}
   return (// class is a keywordin js that's why className is used instead of this in react
     <div className="App">
       {/* <p>Hello @K@^ @ {name}</p>
       <button onClick={() => greet()}> Click Me!</button> */}
       <h1>  TO DO</h1>
-      <br /><br />
-      <h2>Wake Up</h2>
-      <p>Time : 6:00 A.M.</p>
-      <p>Completed</p>
-      <TodoItems title="Lunch" desc="12:35 P.M." status="Pending"/>
-      <TodoItems title="Elite" desc="05:30 P.M." status="Started"/>
+      
+
     </div>
   );
 }
--- frontend/reactspa/src/components/TodoItems.jsx
+++ frontend/reactspa/src/components/TodoItems.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function TodoItems(props) {
   console.log(props);// JS Object File : Props - Not KeyWord but Good Practice
-  let title=props.title;
+  let title=props.title || "default title";
   let desc=props.desc;
   let status=props.status;
-  let time;
+  let time=props.created_at
   return (
     <div className='TodoItem'>
       <h2>{title}</h2>
       <p>{desc}</p>
       <p>{status}</p>
-      <p>{time}</p>
+      <p>Created At:{time}</p>
     </div>
   )
 }
